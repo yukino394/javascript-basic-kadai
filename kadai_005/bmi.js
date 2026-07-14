40729ce8f591af0ae394a98590a93a9ad4508efe
yukino394 <yukinko.m3228@gmail.com>
2026-07-14 16:02:48 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,15 @@
+//変数の宣言
+let bodyWeight;
+
+//値の代入
+bodyWeight='68';
+
+//変数の宣言・値の代入
+let height='1.7';
+
+//コンソールへの出力
+console.log(bodyWeight);
+console.log(height);
+
+
+console.log(68/(1.7*1.7));
